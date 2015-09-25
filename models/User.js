271a82f7312619c501d218b493f56cfa81effd77
jlvivero <josluivivgar@gmail.com>
2015-09-25 11:53:28 -0700

    refactored code so that the route isn't directly on the index, added a one size fits all settings.js

--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var User = mongoose.Schema({
 	user: String,
 	name: String,
-	email: String
+	email: String,
+	phone_number: String
 });
 
 module.exports = mongoose.model('User', User);
---
+++ routes/settings.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var settingsRouter = express.Router();
+var User = require('../models/User');
+
+settingsRouter.get('/', function(req, res) {
+  console.log('setting getter called');
+  var usr = req.params.id;
+  User.findOne({user: usr}, function(err, user) {
+    if (err) {
+      console.log(err);
+      return err;
+    }
+    res.send(user);
+  });
+});
+
+/**************************************************************************************************
+ *	-okay, so the post that needs to go in the frontend needs to contain the following things	    *
+ *	-an id which will contain the student id, m0xxxxx                                			        *						              *
+ *	-body will also contain an email containing the new email									                    *
+ *	-the body can contain number containing the new phone number								                  *
+ *  -Changed the format of the post a little bit so it's easier to test                           *
+ *************************************************************************************************/
+settingsRouter.post('/', function(req, res) {
+  var information = {};
+  information.id = req.body.id;
+  information.email = req.body.email;
+  information.phone = req.body.number;
+  User.findOne({user: information.id}, function(err, user) {
+    if (err) {
+      console.log(err);
+      return err;
+    }
+    user.email = information.email;
+    if (information.phone != null) user.phone_number = information.phone;
+    user.save(function(err) {
+      if (err) return err;
+      res.send(user);
+    });
+  });
+});
+
+module.exports = settingsRouter;
