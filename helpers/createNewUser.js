7a084b643d10e11d3b6ef39bafc54cf0b84952ce
jlvivero <josluivivgar@gmail.com>
2015-10-21 16:33:58 -0700

    added request for approved classes from cetys-api

--- helpers/createNewUser.js
+++ helpers/createNewUser.js
@@ -7,7 +7,7 @@ var createNewUser = function (mat, token) {
     url: "http://cetys-api.herokuapp.com/api/general",
     headers: {
       'access-token': token
-    }}, 
+    }},
     function (err, res, body) {
       console.log('hola');
       console.log(body);
@@ -17,6 +17,29 @@ var createNewUser = function (mat, token) {
       user.save(function (err, u){
         if(err) console.log(err);
         console.log(u);
+      });
+    }
+  );
+  request.get({
+    url: "http://cetys-api.herokuapp.com/api/academicHistory",
+    headers: {
+      'access-token': token
+    }},
+    function (err, res, body){
+      console.log("test");
+      console.log(body);
+      var obj = JSON.parse(body);
+      console.log(obj);
+      User.findOne({user : mat}, function(err,user){
+        if(err){
+          console.log(err);
+          return err;
+        }
+        user.classes = obj.approved; //not sure if you can do that, may have to create a new thing on the models for this
+        user.save(function(err,u){
+          if(err) return err;
+          console.log(u);
+        });
       });
     }
   );
