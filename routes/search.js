91b21f75c21555b6f8e534a016e06c74b8c2a261
jlvivero <josluivivgar@gmail.com>
2015-10-25 16:54:58 -0700

    Added search functionality in backend

---
+++ routes/search.js
@@ -0,0 +1,16 @@
+var request = require('request');
+var mongoose = require('mongoose');
+var User = require('../models/User');
+
+var search = function(req,res){
+  var searchPattern = req.query.name;
+  console.log(searchPattern);
+  User.find({'tutorClasses.0': {$exists: true} , $or: [{'name': new RegExp(searchPattern,'i')},{ 'tutorClasses.name': new RegExp(searchPattern,'i')}]},function(err,tutors){
+    if(err){
+      return err;
+    }
+    res.send(tutors);
+  });
+}
+
+module.exports = search;
