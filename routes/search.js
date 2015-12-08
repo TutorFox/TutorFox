b59ab6847569929f79da0ca2f99eb4ef1715c6e0
Antonio de Luna <tonydelun@gmail.com>
2015-12-07 20:55:39 -0800

    sorted!

--- routes/search.js
+++ routes/search.js
@@ -5,10 +5,11 @@ var User = require('../models/User');
 var search = function(req,res){
   var searchPattern = req.query.name;
   console.log(searchPattern);
-  User.find({'tutorClasses.0': {$exists: true} , $or: [{'name': new RegExp(searchPattern,'i')},{ 'tutorClasses.name': new RegExp(searchPattern,'i')}]},'user name email phone tutorClasses price showGrades', function(err,tutors){
+  User.find({'tutorClasses.0': {$exists: true} , $or: [{'name': new RegExp(searchPattern,'i')},{ 'tutorClasses.name': new RegExp(searchPattern,'i')}]},'user name email phone tutorClasses price showGrades likes', function(err,tutors){
     if(err){
       return err;
     }
+    tutors.sort((a, b) => {return b.likes.length - a.likes.length});
     res.send(tutors);
   });
 }
