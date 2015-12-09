b441c01aa951634fbba3ab8a6ae3788369dee715
just-Ic3 <alejandroelramirez@hotmail.com>
2015-12-08 21:54:10 -0800

    Eliminated About and Contact Us
    No support for u.

--- src/GeneralNavbar.js
+++ src/GeneralNavbar.js
@@ -27,8 +27,6 @@ export default class GeneralNavbar extends React.Component {
               <div id="navbar" className="navbar-collapse collapse">
                 <ul className="nav navbar-nav">
                   <li className="active"><a href="#">Home</a></li>
-                  <li><a href="#about">About</a></li>
-                  <li><a href="#contact">Contact Us</a></li>
                 </ul>
                 <Link to={this.props.loggedIn?'login':'/'}>
                   <button type="button" onClick={this.props.handleClick} className="btn btn-default navbar-btn navbar-right" >
